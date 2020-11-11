04cbe0343ce6f992fd34cf58b0f817eda2d19687
Alexander Whan <whan.alexander@gmail.com>
2020-11-10 23:52:09 -0800

    Updated author name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
-import React from "react"
+import React from 'react';
 
 // Component we're creating
 function Footer() {
-    return (
-        <footer>
-            <p>&copy; 2020 Alex Whan <a href="https://github.com/alex-whan/poke-collector"><i className="fab fa-github fa-2x"></i></a></p>
-        </footer>
-    )
+  return (
+    <footer>
+      <p>
+        &copy; 2020 Alexander Whan{' '}
+        <a href="https://github.com/alex-whan/poke-collector">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
