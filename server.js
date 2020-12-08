125d687577392d0079ef7061f0ae99da8f203341
Alexander Whan <whan.alexander@gmail.com>
2020-12-07 17:24:13 -0800

    Removed unneeded bodyParser import

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const superagent = require('superagent');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/pokemon', getPokemon);
 
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
 
 // Figure out how to read params from request
@@ -29,30 +28,35 @@ async function getPokemon(req, res) {
 
   // console.log(offset+limit);
 
-  for(let i = coercedOffset; i <= coercedOffset + coercedLimit - 1; i++){
+  for (let i = coercedOffset; i <= coercedOffset + coercedLimit - 1; i++) {
     console.log('this is i:', i);
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promiseArray.push(superagent.get(url));
   }
 
   const pokemonResponses = await Promise.all(promiseArray);
-  const pokemon = pokemonResponses.map(({body}) => ({
+  const pokemon = pokemonResponses.map(({ body }) => ({
     name: body.name ? body.name : 'Name not available.',
     url: body.species.url ? body.species.url : 'URL not available.',
-    pokedex_number: body.species.url.split('/')[body.species.url.split('/').length - 2],
-    image: body.sprites.front_default ? body.sprites.front_default : 'Image not available.',
-    type1: body.types[0].type.name ? body.types[0].type.name : 'Type 1 not available.',
-    // if(body.types.length > 1){ 
+    pokedex_number: body.species.url.split('/')[
+      body.species.url.split('/').length - 2
+    ],
+    image: body.sprites.front_default
+      ? body.sprites.front_default
+      : 'Image not available.',
+    type1: body.types[0].type.name
+      ? body.types[0].type.name
+      : 'Type 1 not available.',
+    // if(body.types.length > 1){
     //   // checks to see if Pokemon has a second type
     //   type2: body.types[1].type.name
     // } else {
     //   type2: null
     // }
-
   }));
-  
+
   return res.send(pokemon);
- };
+}
 
 app.get('/', function (req, res) {
   console.log('Hello World');
@@ -63,4 +67,4 @@ app.get('/', function (req, res) {
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
-})
+});
