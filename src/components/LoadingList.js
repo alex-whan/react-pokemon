f779b82187f3352bad6df98bef8309b41521078e
Alexander Whan <whan.alexander@gmail.com>
2020-12-24 11:41:26 -0800

    Clarified comments on LoadingList Hook

--- src/components/LoadingList.js
+++ src/components/LoadingList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function LoadingList (Component){
-    return function LoadingComponent({ isLoading, ...props }) {
-        if (!isLoading) return <Component {...props} />;
-        return (
-            <p>Loading...</p>
-        )
-    }
+// This is a hook
+
+function LoadingList(Component) {
+  return function LoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return <p>Loading...</p>;
+  };
 }
 
-export default LoadingList
+export default LoadingList;
